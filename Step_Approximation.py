ee4a92826ec0e61bc9cd9719bd8487746f195966
UtkarshR8j <37337463+UtkarshR8j@users.noreply.github.com>
2021-02-03 19:18:33 +0530

    Add files via upload

--- Step_Approximation.py
+++ Step_Approximation.py
@@ -3,21 +3,29 @@ from sympy import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=symbols("x")    # declares the normal variable x as a Sympy symbol
+x=symbols("x")
 s = input("")
-exp1 = exp(sympify(s))   #converts the string function input  by the user to mathematical form 
+exp1 = exp(sympify(s))
 kval=[]
 tval=[]
 yval=[]
 
-for y in np.arange(0,1,0.1):  #for varying value between 0 to 1 with 0.1 increament of the input argument of the function input by the user
-    for k in np.arange(1,0.001,-0.01): # varying step size
-        t= ((diff(exp1,x)).subs(x,y)) - ((exp1.subs(x,(y+k))-exp1.subs(x,y))/k)
-        #print (t.evalf())   #increases load on interpreter 
-        kval.append(k)       #looping using index create an error "out of range"
+for y in np.arange(1,-1,-0.01):
+    for k in np.arange(1,-1,-0.01):
+
+        t= (((diff(exp1,x)).subs(x,y)) - ((exp1.subs(x,(y+k))-exp1.subs(x,y))/k))
+        u =diff(exp1,x).subs(x,y)
+        t=t/u
+        #print (t.evalf())
+        kval.append(k)
         tval.append(t.evalf())
     yval.append(y)
         
         
-plt.plot(tval,kval)         #using matplotlib for plotting 2-d graphs
+plt.plot(tval,kval)
+plt.xlabel('Error%')
+plt.ylabel('Step_Size')
+plt.xlim(-1,1)
+#plt.plot(y,tval)
+
 plt.show()
