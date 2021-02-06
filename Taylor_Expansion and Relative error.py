c1b04567121014c38deaaac1637dca920407276f
UtkarshR8j <37337463+UtkarshR8j@users.noreply.github.com>
2021-02-07 00:16:55 +0530

    Create Taylor_Expansion and Relative error.py

---
+++ Taylor_Expansion and Relative error.py
@@ -0,0 +1,34 @@
+import sympy
+from sympy import *
+import numpy as np
+x=symbols("x")
+s = input()
+l=10
+exp1 = (sympify(s))
+expf = Function('expf')
+expf=0
+exp2=exp1
+#print(exp2)
+
+for n in np.arange(1,l,1):
+    k=n        
+    for k in np.arange(n,0,-1):
+        exp2 = diff(exp2)/k
+    ta=exp2*(x**n)
+    expf=ta+expf
+    exp2=exp1
+
+#print(expf)
+p1=plot(expf)
+p1.show
+
+import matplotlib.pyplot as plt
+
+errval=[]
+yval=[]
+for y in np.arange(-10,10,0.1):
+    err = (exp1.subs(x,y)-expf.subs(x,y))/exp1.subs(x,y)
+    yval.append(y)
+    errval.append(err)
+    
+plt.plot(yval,errval)
